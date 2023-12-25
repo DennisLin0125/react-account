b74416a4db125541600be615b9d0e49ed5c58cd4
Dennis <740125aa@gmail.com>
2023-12-25 17:05:29 +0800

    將數據按月分組 完成

--- src/pages/Month/index.jsx
+++ src/pages/Month/index.jsx
@@ -1,10 +1,21 @@
 import {NavBar,DatePicker} from "antd-mobile";
 import './index.scss'
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import classNames from "classnames";
 import dayjs from 'dayjs'
+import { useSelector } from "react-redux";
+import _ from 'lodash'
 
 const Month = () => {
+  // 按月分組
+  const billList = useSelector(state => state.bill.billList)
+  
+  const monthGroup = useMemo(() => {
+    return _.groupBy(billList,(item)=>dayjs(item.date).format('YYYY-MM'))
+  }, [billList]);
+  
+  console.log('@',monthGroup)
+  
   // 控制彈框
   const [dataVisible,setDataVisible] = useState(false)
   // 控制時間
