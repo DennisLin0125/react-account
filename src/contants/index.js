52e02d1eb3c1e63a0bda36ffdcce331509b6cd61
Dennis <740125aa@gmail.com>
2023-12-25 21:33:01 +0800

    單日帳單列表渲染 完成

---
+++ src/contants/index.js
@@ -0,0 +1,75 @@
+export const billListData = {
+  pay: [
+    {
+      type: "foods",
+      name: "餐飲",
+      list: [
+        {type: "food", name: "餐費"},
+        {type: "drinks", name: "酒水飲料"},
+        {type: "dessert", name: "甜食"},
+      ],
+    },
+    {
+      type: "taxi",
+      name: "出行交通",
+      list: [
+        {type: "taxi", name: "車費"},
+        {type: "longDistance", name: "旅行票費"},
+      ],
+    },
+    {
+      type: "recreation",
+      name: "休閒娛樂",
+      list: [
+        {type: "bodybuilding", name: "健身"},
+        {type: "game", name: "休閒玩樂"},
+        {type: "audio", name: "媒體影音"},
+        {type: "travel", name: "旅遊度假"},
+      ],
+    },
+    {
+      type: "daily",
+      name: "日常支出",
+      list: [
+        {type: "clothes", name: "衣服"},
+        {type: "bag", name: "包包"},
+        {type: "book", name: "知識學習"},
+        {type: "promote", name: "能量提升"},
+        {type: "home", name: "家庭布置"},
+      ],
+    },
+    {
+      type: "other",
+      name: "其他支出",
+      list: [
+        {type: "community", name: "管理費"},
+      ],
+    }
+  ],
+  income: [
+    {
+      type: "professional",
+      name: "其他支出",
+      list: [
+        {type: "salary", name: "薪水"},
+        {type: "overtimepay", name: "加班"},
+        {type: "bonus", name: "獎金"},
+      ],
+    },
+    {
+      type: "other",
+      name: "其他支出",
+      list: [
+        {type: "financial", name: "理財收入"},
+        {type: "cashgift", name: "禮金收入"},
+      ],
+    }
+  ]
+}
+
+export const billTypeToName = Object.keys(billListData).reduce((pre,key)=>{
+  billListData[key].forEach(bill=>{
+    bill.list.forEach(item=>pre[item.type] = item.name)
+  })
+  return pre
+},{})
--- src/pages/Month/components/DayBill/index.jsx
+++ src/pages/Month/components/DayBill/index.jsx
@@ -1,46 +1,68 @@
 import { useMemo } from "react";
 import classNames from "classnames";
 import './index.scss'
+import { billTypeToName } from "@/contants";
 
-const DayBill = ({date,billList}) => {
+const DayBill = ({date, billList}) => {
   const dayResult = useMemo(() => {
-    if (billList){
-      const pay = billList.filter(item=>item.type==='pay').reduce((a,c)=>a+c.money,0)
-      const income = billList.filter(item=>item.type==='income').reduce((a,c)=>a+c.money,0)
-      return{
+    if (billList) {
+      const pay = billList.filter(item => item.type === 'pay').reduce((a, c) => a + c.money, 0)
+      const income = billList.filter(item => item.type === 'income').reduce((a, c) => a + c.money, 0)
+      return {
         pay,
         income,
         total: pay + income
       }
-    }else {
-      return{
-        pay:0,
-        income:0,
+    } else {
+      return {
+        pay: 0,
+        income: 0,
         total: 0
       }
     }
   }, [billList]);
   
-  return(
+  return (
     <div className={classNames('dailyBill')}>
       <div className="header">
         <div className="dateIcon">
           <span className="date">{date}</span>
+          <span className={classNames('arrow')}></span>
+        </div>
+        
+        <div className="oneLineOverview">
+          <div className="pay">
+            <span className="type">支出</span>
+            <span className="money">{dayResult.pay.toFixed(2)}</span>
+          </div>
+          <div className="income">
+            <span className="type">收入</span>
+            <span className="money">{dayResult.income.toFixed(2)}</span>
+          </div>
+          <div className="balance">
+            <span className="money">{dayResult.total.toFixed(2)}</span>
+            <span className="type">結餘</span>
+          </div>
         </div>
       </div>
-      <div className="oneLineOverview">
-        <div className="pay">
-          <span className="type">支出</span>
-          <span className="money">{dayResult.pay.toFixed(2)}</span>
-        </div>
-        <div className="income">
-          <span className="type">收入</span>
-          <span className="money">{dayResult.income.toFixed(2)}</span>
-        </div>
-        <div className="balance">
-          <span className="money">{dayResult.total.toFixed(2)}</span>
-          <span className="type">結餘</span>
-        </div>
+      {/*   單日列表*/}
+      <div className="billList">
+        {
+          billList.map(item=>{
+            return(
+              <div className="bill" key={item.id}>
+                <div className="detail">
+                  <div className="billType">{billTypeToName[item.useFor]}</div>
+                </div>
+                <div className={classNames('money', (item.type))}>
+                  {
+                    item.money.toFixed(2)
+                  }
+                </div>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   )
