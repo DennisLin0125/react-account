f6d27c60ec38cbb97c3152f557d4730002d3bd0e
Dennis <740125aa@gmail.com>
2023-12-25 13:18:14 +0800

    主題配置完成

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
+import { Button } from "antd-mobile";
 
 const Layout = () => {
   return (
     <div>
       <Outlet/>
       我是layout
+      {/* 測試 */}
+      <Button color="primary">測試全局</Button>
+      <div className='purple'>
+        <Button color="primary">測試局部</Button>
+      </div>
     </div>
   )
 }
